983ce9de876ce0b3bb8d4cf197bd111a254f2dc8
NickyAdmin <79171151+NickyAdmin@users.noreply.github.com>
2021-04-04 11:19:12 +0100

    Add files via upload

--- NickysFixer/src/main/java/me/nickyadmin/nickysfixer/antilag/redstone.java
+++ NickysFixer/src/main/java/me/nickyadmin/nickysfixer/antilag/redstone.java
@@ -5,7 +5,8 @@ import me.nickyadmin.nickysfixer.utils.GetTps;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPistonEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
 
 public class redstone implements Listener {
         private final Main plugin;
@@ -15,7 +16,7 @@ public class redstone implements Listener {
         }
 
         @EventHandler
-        public void onRedstone(BlockPistonEvent event) {
+        public void onRedstone(BlockPistonRetractEvent event) {
             FileConfiguration config = plugin.getConfig();
             if (config.getBoolean("limit-redstone")) {
             if (GetTps.getTps() <= config.getInt("tps-to-disable-redstone")) {
@@ -24,4 +25,14 @@ public class redstone implements Listener {
 
             }
         }
+    @EventHandler
+    public void onRedstone(BlockPistonExtendEvent event) {
+        FileConfiguration config = plugin.getConfig();
+        if (config.getBoolean("limit-redstone")) {
+            if (GetTps.getTps() <= config.getInt("tps-to-disable-redstone")) {
+                event.setCancelled(true);
+            }
+
+        }
+    }
     }
